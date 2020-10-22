7e9bdf63a04cecb60c19e69f83de62eb3ae091eb
Walter <vignawalter@gmail.com>
2020-10-22 17:42:37 +0200

    finalisation

--- models/password.js
+++ models/password.js
@@ -4,7 +4,7 @@ const passwordSchema = new passwordValidator();
 
 passwordSchema
 .is().min(7)                                    // minimum 7 caractères
-.is().max(100)                                  // maximum 25 caractères
+.is().max(20)                                   // maximum 20 caractères
 .has().uppercase()                              // Doit avoir 1 lettre majuscule minimum
 .has().lowercase()                              // Doit avoir 1 lettre minuscule minimum
 .has().digits(2)                                // Doit avoir 2 chiffres minimum
